6fa2a2199d04a442203ab675296c94a3e26d2088
Martin Molema <martin@molema.org>
2024-03-30 10:59:48 +0100

    Added prime factorisation

--- lightswitchproblem/index.js
+++ lightswitchproblem/index.js
@@ -12,8 +12,11 @@ let elBtnReset;
 let elBtnStop;
 let timer;
 let elIterationSpeed;
+let elPrimefactors;
 
 let width, height, nrOfItems;
+
+let primes;
 
 function setup() {
     elNrOfItems = document.querySelectorAll("input[name='nrOfItems']")[0];
@@ -26,7 +29,9 @@ function setup() {
     elBtnNextValue = document.getElementById("startIteration");
     elBtnReset = document.getElementById("reset");
     elBtnStop = document.getElementById("stop");
+    elPrimefactors = document.getElementById("primefactors");
 
+    document.getElementById("settings").addEventListener("submit", handleSubmit);
     document.getElementById("settings").addEventListener("submit", handleSubmit);
     elBtnNextValue.addEventListener("click", StartIteration);
     elBtnStop.addEventListener("click", StopIteration);
@@ -34,27 +39,30 @@ function setup() {
 
     console.log(elNrOfItems, elHeight.value, elHeight.value, elContents);
     ResetFieldValues();
+
+    findAllPrimesUpTo(nrOfItems)
 }
 
 function handleSubmit(event) {
     event.stopPropagation();
     event.preventDefault();
     ResetFieldValues();
+    findAllPrimesUpTo(elNrOfItems)
 }
 
 function ResetFieldValues() {
     elContents.innerHTML = '';
-    nrOfItems = elNrOfItems.value;
-    width = elWidth.value;
+    nrOfItems = parseInt(elNrOfItems.value);
+    width = parseInt(elWidth.value);
     height = (nrOfItems / width);
-    elHeight.value = height;
+    elHeight.value = parseInt(height);
+
     console.log(nrOfItems, width, height);
+
     ResetField();
     elContents.addEventListener('animationend', (event) => {
         const element = event.target;
-        console.log(element.childNodes[0].textContent);
         element.classList.remove('currentlyChanging');
-        console.log(element.classList);
     });
 }
 
@@ -107,6 +115,8 @@ function StopIteration() {
 
 
 function ProcessForNumber(counter, iterationSpeed) {
+    elPrimefactors.textContent = determinePrimeFactorsForNumber(counter).join(" x ")
+
     let items = document.querySelectorAll("TD");
     items.forEach(elTableCell => {
         const nr = elTableCell.dataset.number;
@@ -120,6 +130,58 @@ function ProcessForNumber(counter, iterationSpeed) {
 
             elTableCell.innerHTML = `<p>${nr}</p><p>(${nrOfChanges})</p>`;
 
+
         }
     });
+}
+
+/* Use sif to find all primes up to the number of items given */
+function findAllPrimesUpTo(num) {
+    // fill list with numbers
+    const numbers = [...Array(num + 1).keys().map(x => x + 1)];
+    numbers[0] = 0; // 1 is not a prime number;
+    for (let i = 2; i <= num; i++) {
+        let j = i * i;
+        while (j <= num) {
+            numbers[j - 1] = 0;
+            j = j + i;
+        }
+    }
+    primes = numbers.filter(x => x !== 0);
+}
+
+function isPrime(num) {
+    return primes.findIndex(x => x === num) !== -1;
+}
+
+function determinePrimeFactorsForNumber(num) {
+    const originalNumber = num;
+    const factors = [];
+    const halfway = (num % 2) === 0 ? (num / 2) : (num / 2) + 1;
+    const numberOfPrimes = primes.length;
+
+    if (isPrime(num)) {
+        factors.push(num);
+        factors.push(1);
+    } else {
+        let primeIndex = 0;
+        while (primeIndex < numberOfPrimes && num !== 1) {
+            let prime = primes[primeIndex];
+            if (num % prime === 0) {
+                // one factor is the prime found
+                factors.push(prime);
+
+                num = num / prime;
+
+                primeIndex = 0;
+            }
+            else {
+                primeIndex++;
+            }
+        }
+        factors.push(1);
+    }
+
+    console.log(originalNumber, factors.join("x"))
+    return factors;
 }
