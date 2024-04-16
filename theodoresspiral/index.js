8dd8e99017a5b87541335f9da07e551f8938b1ac
Martin Molema <martin@molema.org>
2024-04-16 09:41:01 +0200

    working version of the Theodore's Spiral

--- theodoresspiral/index.js
+++ theodoresspiral/index.js
@@ -2,11 +2,41 @@ window.onload = function () {
     setup();
 }
 const SVG_NS = "http://www.w3.org/2000/svg";
-let elSVG = document.getElementById("contents");
-const UNIT_IN_PIXEL = 1;
 
-const ORIGIN_X = 400;
-const ORIGIN_Y = 400;
+let elSvgCanvas;
+let elAxesMarkers;
+let elSVGDrawing;
+let elSlider;
+let elSliderValue;
+
+// the current number of iterations requested by the user
+let nrOfIterations;
+
+// dimensions of the complete SVG
+let svgWidth;
+let svgHeight;
+
+let drawingSpaceMargin = 10;
+
+// the number of markers on the positive or negative part of an axis
+let desiredNrOfMarkers = 10;
+let actualNumberOfMarkers = 10;
+
+// the maximum radius to draw: this is in fact the square root of the current number of iterations
+let maxRadius;
+
+// the distance between to values on the axis; so this is NOT the amount of pixels
+let markerDistance;
+
+// the multiplying factor to scale a radius on the SVG.
+let UNIT_IN_PIXEL = 1;
+
+// the location of the origin.
+let ORIGIN_X = 400;
+let ORIGIN_Y = 400;
+
+// list of points
+let listOfPoints = [];
 
 
 class Point {
@@ -21,47 +51,215 @@ class Point {
 }
 
 function setup() {
-    let previousPoint = new Point(1, 0);
-    for (let i = 1; i < 400; i++) {
+    elSvgCanvas = document.getElementById("canvas");
+    elSVGDrawing = document.getElementById("contents");
+    elSlider = document.getElementById("slider");
+    elSliderValue = document.getElementById("sliderValue");
+    elAxesMarkers = document.getElementById("axesmarkers");
+
+    svgWidth = elSvgCanvas.width.baseVal.value - 2 * drawingSpaceMargin;
+    svgHeight = elSvgCanvas.height.baseVal.value - 2 * drawingSpaceMargin;
+
+    elSlider.addEventListener("input", function (event) {
+        nrOfIterations = parseInt(elSlider.value);
+        updateParametersFromScreenInput();
+        calculateDrawingUnits();
+        draw();
+    })
+
+    nrOfIterations = 10;
+    updateParametersFromScreenInput();
+    calculateDrawingUnits();
+    draw();
+}
+
+function updateParametersFromScreenInput() {
+    elSlider.value = nrOfIterations;
+    elSliderValue.innerText = nrOfIterations.toString();
+}
+
+function myround(value, step) {
+    step || (step = 1.0);
+    var inv = 1.0 / step;
+    return Math.round(value * inv) / inv;
+}
+
+function calculateDrawingUnits() {
+    ORIGIN_X = drawingSpaceMargin + svgWidth / 2;
+    ORIGIN_Y = drawingSpaceMargin + svgHeight / 2;
+
+    // create a new list of points
+    listOfPoints = [];
+
+    // a bit hacky:prepare the first point so a valid "previous point" can be given to the FOR-loop handling the rest.
+    listOfPoints.push(new Point(1, 0));
+    listOfPoints.push(new Point(1, 1));
+
+    // remember the last point so a proper triangle can be constructed.
+    let previousPoint = new Point(1, 1);
+
+    let angle = (45 / 360) * Math.PI * 2;
+
+    for (let i = 2; i <= nrOfIterations; i++) {
+        // now calculate the angle. fill an array with a range first
+        const parts = Array.from(Array(i - 1).keys());
+
+        // now calculate a sum using the Reduce method to calculate the angles
+        // const angle = parts.reduce((sumValue, currentValue) => sumValue + Math.atan(1 / Math.sqrt(currentValue)), 0);
+
+        angle += Math.atan(1 / Math.sqrt(i));
+
+        // calculate the radius: the Square Root of the current iteration
+        const radius = Math.sqrt(i);
+
+        // translate from Polar to Carthesian (https://www.mathcentre.ac.uk/resources/uploaded/mc-ty-polar-2009-1.pdf)
+        let x = radius * Math.cos(angle);
+        let y = radius * Math.sin(angle);
+
+        // Add the point
+        const newPoint = new Point(x, y);
+        listOfPoints.push(newPoint);
+        previousPoint = newPoint;
+    }
+
+    const maxX = listOfPoints.map(p => p.x).reduce((previousValue, currentValue) => Math.abs(currentValue) > previousValue ? Math.abs(currentValue) : previousValue, 0);
+    const maxY = listOfPoints.map(p => p.y).reduce((previousValue, currentValue) => Math.abs(currentValue) > previousValue ? Math.abs(currentValue) : previousValue, 0);
+
+    const maxPosition = Math.max(maxX, maxY);
+
+    // calculate the value between ticks: each position is determined by
+    markerDistance = maxPosition / desiredNrOfMarkers;
+    markerDistance = myround(markerDistance, 0.1);
+    actualNumberOfMarkers = maxPosition / markerDistance;
+
+    // calculate the multiplication factor: the width is halved (because we only need one part (e.g. positive) of the
+    // axis. Then the maximum position on that axis is used to divide the available pixels by the needed space (in calculation space)
+    UNIT_IN_PIXEL = (svgWidth / 2) / maxPosition;
+}
+
+function drawAxesMarkers() {
+    elAxesMarkers.innerHTML = '';
+
+    // calculate the visual spacing of the markers
+    let stepX = svgWidth / (2 * actualNumberOfMarkers);
+    let stepY = svgHeight / (2 * actualNumberOfMarkers);
+
+    // now create an integer number so the for-loop can actually place the correct number of markers
+    const nrOfMarkersToPlace = Math.floor(actualNumberOfMarkers);
+
+    for (let tickNrX = -nrOfMarkersToPlace; tickNrX <= nrOfMarkersToPlace; tickNrX++) {
+        // create the marker line
+        const marker = document.createElementNS(SVG_NS, "line");
+        const x1 = ORIGIN_X + tickNrX * stepX;
+        const y1 = ORIGIN_Y - 3;
+        const x2 = x1;
+        const y2 = y1 + 6;
+
+        marker.setAttribute('x1', x1);
+        marker.setAttribute('y1', y1);
+        marker.setAttribute('x2', x2);
+        marker.setAttribute('y2', y2);
+        marker.classList.add("tick");
+        elAxesMarkers.appendChild(marker);
+
+        const txt = document.createElementNS(SVG_NS, "text");
+        txt.setAttribute('x', x1);
+        txt.setAttribute('y', y2 + 10);
+        txt.classList.add("tick");
+        txt.classList.add("horizontal");
+
+        const markerValue = myround(tickNrX * markerDistance, 0.1);
+        txt.textContent = markerValue.toString();
+        elAxesMarkers.appendChild(txt);
+    }
+
+    // now place the Y-axis lines. Remember that the Y-axis of the carthesian system is reversed compared to the SVG-coordinate system!
+    for (let tickNrY = nrOfMarkersToPlace; tickNrY >= -nrOfMarkersToPlace; tickNrY--) {
+        const marker = document.createElementNS(SVG_NS, "line");
+
+        const x1 = ORIGIN_X - 3;
+        const y1 = ORIGIN_Y - tickNrY * stepX;
+        const x2 = x1 + 6;
+        const y2 = y1;
+
+        marker.setAttribute('x1', x1);
+        marker.setAttribute('y1', y1);
+        marker.setAttribute('x2', x2);
+        marker.setAttribute('y2', y2);
+        marker.classList.add("tick");
+        elAxesMarkers.appendChild(marker);
+
+        const txt = document.createElementNS(SVG_NS, "text");
+        txt.setAttribute('x', x1);
+        txt.setAttribute('y', y1 + 4);
+        txt.classList.add("tick");
+        txt.classList.add("vertical");
+
+        const markerValue = myround(tickNrY * markerDistance, 0.1);
+        txt.textContent = markerValue.toString();
+        elAxesMarkers.appendChild(txt);
+    }
+
+}
+
+function mapXPositionToCanvas(x) {
+    return ORIGIN_X + x * UNIT_IN_PIXEL
+}
+
+function mapYPositionToCanvas(y) {
+    return ORIGIN_Y - y * UNIT_IN_PIXEL;
+}
+
+function mapCoordinateToCanvas(x, y) {
+    return new Point(mapXPositionToCanvas(x), mapYPositionToCanvas(y));
+}
+
+function mapPointToCanvas(p) {
+    return mapCoordinateToCanvas(p.x, p.y);
+}
+
+function draw() {
+    elSVGDrawing.innerHTML = '';
+
+    drawAxesMarkers();
+    let previousPoint = listOfPoints[0];
+
+    for (let i = 0; i < listOfPoints.length; i++) {
+
+        const currentPoint = listOfPoints[i];
+
         const polygon = document.createElementNS(SVG_NS, "polygon");
 
         // create an empty array that will be expanded to the set of points
         const points = [];
 
         // push Origin (0,0)
-        points.push(new Point(0,0));
+        points.push(mapCoordinateToCanvas(0, 0));
 
         // add the point from the previous iteration
-        points.push(previousPoint);
+        points.push(mapPointToCanvas(previousPoint));
 
-        // now calculate the angle. fill an array with a range first
-        const parts = Array.from(Array(i-1).keys());
-        // now calculate a sum using the Reduce method to calculate the angles
-        const angle = 2* Math.PI - parts.reduce((sumValue, currentValue) => sumValue + Math.atan(1 / Math.sqrt(currentValue)), 0);
-
-        // calculate the radius: the Square Root of the current iteration
-        const radius = Math.sqrt(i);
-
-        // translate from Polar to Carthesian (https://www.mathcentre.ac.uk/resources/uploaded/mc-ty-polar-2009-1.pdf)
-        let x = previousPoint.x + radius * Math.cos(angle);
-        let y = previousPoint.y + radius * Math.sin(angle);
+        const x = mapXPositionToCanvas(currentPoint.x);
+        const y = mapYPositionToCanvas(currentPoint.y);
 
         // Add the point
         const newPoint = new Point(x, y);
+        points.push(newPoint);
 
         // put a small dot on the outer rim of the spiral for this iteration
         const circle = document.createElementNS(SVG_NS, "circle");
-        circle.setAttribute("cx", ORIGIN_X + x * UNIT_IN_PIXEL);
-        circle.setAttribute("cy", ORIGIN_Y + y * UNIT_IN_PIXEL);
+        circle.setAttribute("cx", x);
+        circle.setAttribute("cy", y);
         circle.setAttribute("r", 2);
         circle.classList.add('dot');
-        elSVG.appendChild(circle);
+        elSVGDrawing.appendChild(circle);
 
-        points.push(newPoint);
+        const listOfPointsAsString = points.map(m => `${m.x},${m.y}`).join(' ');
 
-        previousPoint = newPoint;
-        polygon.setAttribute('class', `segment nr_${i}`)
-        polygon.setAttribute("points", points.map(m => `${ORIGIN_X + m.x * UNIT_IN_PIXEL},${ORIGIN_Y + m.y * UNIT_IN_PIXEL}`).join(' '));
-        elSVG.appendChild(polygon);
+        polygon.setAttribute('class', `segment nr_${i}`);
+        polygon.setAttribute("points", listOfPointsAsString);
+        elSVGDrawing.appendChild(polygon);
+
+        previousPoint = currentPoint;
     }
 }
