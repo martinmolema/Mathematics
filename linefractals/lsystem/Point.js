4a1153162dd0fd0e13597638872eb398c8ae21d7
Martin Molema <martin@molema.org>
2024-05-13 09:40:44 +0200

    First steps in creating a generic L-rewrite system

---
+++ linefractals/lsystem/Point.js
@@ -0,0 +1,11 @@
+export class Point {
+    x;
+    y;
+    iterationNr;
+
+    constructor(x, y, iterationNr) {
+        this.x = x;
+        this.y = y;
+        this.iterationNr = iterationNr;
+    }
+}
